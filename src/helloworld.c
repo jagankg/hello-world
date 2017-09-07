d893c0761a44c810eb2a8058d11635dd33f85d27
jagankg <jagankg.career@gmail.com>
2017-09-07 23:00:12 +0530

    passing arguments to app

--- src/helloworld.c
+++ src/helloworld.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main ()
+int main(int argc, char **argv)
 {
+   int ui_numparam = argc;
+   int ui_count = 0;
    printf("Hello World\n");
+   printf("Num Parameter Passed %d\n", ui_numparam);
+
+   for (ui_count = 0; ui_count < ui_numparam; ui_count++)
+   {
+      printf("ARG[%d] = %s\n", ui_count, argv[ui_count]);
+   }
    return 0;
 }
